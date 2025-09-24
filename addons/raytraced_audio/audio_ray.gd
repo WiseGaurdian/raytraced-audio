a34885ff77669680ecd41cfa9034d99065f9593e
WiseGaurdian <77251348+WiseGaurdian@users.noreply.github.com>
2025-09-24 02:58:50 -0400

    Update audio_ray.gd
    Adding more regions while searching for resource save bug

--- addons/raytraced_audio/audio_ray.gd
+++ addons/raytraced_audio/audio_ray.gd
@@ -4,6 +4,9 @@ extends RayCast3D
 # shhhhhh its okay
 # ( -  ͜ʖ -)☞(ʘ_ʘ; )
 
+
+#Variables
+#region
 var cast_dist: float = 0.0
 var max_bounces: int = 1
 var ray_scatter: Callable
@@ -17,7 +20,7 @@ var escaped: bool = false
 var escape_dir: Vector3 = Vector3.ZERO
 
 var _space_state: PhysicsDirectSpaceState3D
-
+#endregion
 
 func _init(raycast_dist: float, max_bounce_count: int) -> void:
 	top_level = true
@@ -47,6 +50,7 @@ func update():
 	bounces += 1
 
 	# Escaped outside
+	#region
 	if !is_colliding():
 		escaped = true
 		global_position += target_position
@@ -55,23 +59,29 @@ func update():
 			var has_line_of_sight: bool = _cast_ray(player.global_position).is_empty()
 			player._lowpass_rays_count += int(has_line_of_sight)
 		return
+	#endregion
 
 	# Bounce
+	#region
 	var hit_pos: Vector3 = get_collision_point()
 	var normal: Vector3 = get_collision_normal()
 	global_position = hit_pos + normal * 0.1
 	target_position = target_position.bounce(normal)
 	has_bounced_this_tick = true
+	#endregion
 
 	# Muffle ray: Check for line of sight with audio players
+	#region
 	for player: RaytracedAudioPlayer3D in get_tree().get_nodes_in_group(RaytracedAudioPlayer3D.ENABLED_GROUP_NAME):
 		var has_line_of_sight: bool = _cast_ray(player.global_position).is_empty()
 		player._lowpass_rays_count += int(has_line_of_sight)
 		# Same as:
 		# if has_line_of_sight:
 		# 	player.lowpass_rays_count += 1
+	#endregion
 
 	# Echo ray: Check for line of light with listener
+	#region
 	# Optimization: no need to raycast for first echo bounce
 	if bounces == 1:
 		echo_dist = hit_pos.distance_to(owner.global_position)
@@ -82,6 +92,7 @@ func update():
 		echo_dist = hit_pos.distance_to(owner.global_position)
 		echo_count += 1
 		escape_dir = owner.global_position.direction_to(hit_pos)
+	#endregion
 
 
 # Return to the listener with a random direction
